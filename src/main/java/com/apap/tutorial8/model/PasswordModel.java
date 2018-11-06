f5f9fb709e249cce3338701f8db8259936b16f83
Nurdela <nurdela.ardianyah@gmail.com>
2018-11-06 20:56:29 +0700

    first commit, latihan done

---
+++ src/main/java/com/apap/tutorial8/model/PasswordModel.java
@@ -0,0 +1,25 @@
+package com.apap.tutorial8.model;
+
+public class PasswordModel {
+	private String passwordLama;
+	private String passwordBaru;
+	private String passwordBaruKonf;
+	public String getPasswordLama() {
+		return passwordLama;
+	}
+	public void setPasswordLama(String passwordLama) {
+		this.passwordLama = passwordLama;
+	}
+	public String getPasswordBaru() {
+		return passwordBaru;
+	}
+	public void setPasswordBaru(String passwordBaru) {
+		this.passwordBaru = passwordBaru;
+	}
+	public String getPasswordBaruKonf() {
+		return passwordBaruKonf;
+	}
+	public void setPasswordBaruKonf(String passwordBaruKonf) {
+		this.passwordBaruKonf = passwordBaruKonf;
+	}
+}
---
+++ src/main/java/com/apap/tutorial8/service/UserRoleService.java
@@ -0,0 +1,11 @@
+package com.apap.tutorial8.service;
+
+import com.apap.tutorial8.model.UserRoleModel;
+
+public interface UserRoleService {
+	UserRoleModel addUser(UserRoleModel user);
+	String encrypt(String password);
+	UserRoleModel findUserByUsername(String name);
+	void changePassword(UserRoleModel user, String newPassword);
+	
+}
